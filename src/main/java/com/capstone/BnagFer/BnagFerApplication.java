aab82e6a389d3ec2af39a1eee67f7bfdb0e2e10b
gomin0 <gomin0981113@naver.com>
2024-02-10 23:01:46 +0900

    :sparkles: feat: 초기 셋팅 (#1)

---
+++ src/main/java/com/capstone/BnagFer/BnagFerApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.BnagFer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnagFerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnagFerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/BnagFer/BnagFerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.BnagFer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnagFerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
