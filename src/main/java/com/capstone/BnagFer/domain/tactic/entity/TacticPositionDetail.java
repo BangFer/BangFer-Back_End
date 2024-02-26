e61a91732c3468177e3415d6e98fc58935885127
jiiiiiw <kijiwi1@naver.com>
2024-02-26 20:12:18 +0900

    :sparkles: feat: TacticPositionDetail 엔티티 클래스 작성 (#10)

---
+++ src/main/java/com/capstone/BnagFer/domain/tactic/entity/TacticPositionDetail.java
@@ -0,0 +1,29 @@
+package com.capstone.BnagFer.domain.tactic.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TacticPositionDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "detail_id")
+    private Long detailId;
+
+    @ManyToOne
+    @JoinColumn(name = "tactic_id", referencedColumnName = "tactic_id")
+    private Tactic tactic;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "position")
+    private Position position;
+
+    @Column(name = "position_description")
+    private String positionDescription;
+
+}
