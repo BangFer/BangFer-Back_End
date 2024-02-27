90b197ca7290fb932950df79de2cd9bb05fafe56
gomin0 <gomin0981113@naver.com>
2024-02-27 18:02:25 +0900

    :recycle: refactor: SecurityConfig 파일 리팩토링 (#21)

---
+++ src/main/java/com/capstone/BnagFer/domain/accounts/jwt/config/SecurityConfig.java
@@ -0,0 +1,103 @@
+package com.capstone.BnagFer.domain.accounts.jwt.config;
+
+import com.capstone.BnagFer.domain.accounts.jwt.filter.JwtAuthenticationFilter;
+import com.capstone.BnagFer.domain.accounts.jwt.exception.JwtAccessDeniedHandler;
+import com.capstone.BnagFer.domain.accounts.jwt.exception.JwtAuthenticationEntryPoint;
+import com.capstone.BnagFer.domain.accounts.jwt.filter.JwtExceptionFilter;
+import com.capstone.BnagFer.domain.accounts.jwt.util.JwtProvider;
+import com.capstone.BnagFer.global.config.CorsConfig;
+import com.capstone.BnagFer.global.config.encoder.Pbkdf2PasswordEncoder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final String[] swaggerUrls = {"/swagger-ui/**", "/v3/**"};
+    private final String[] authUrls = {"/", "/accounts/signup/**", "/accounts/login/**",
+            "/api/v1/auth"};
+    private final String[] allowedUrls = Stream.concat(Arrays.stream(swaggerUrls), Arrays.stream(authUrls))
+            .toArray(String[]::new);
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtProvider jwtProvider;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new Pbkdf2PasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors(cors -> cors
+                        .configurationSource(CorsConfig.apiConfigurationSource()));
+
+        // csrf disable
+        http
+                .csrf(AbstractHttpConfigurer::disable);
+
+        // form 로그인 방식 disable
+        http
+                .formLogin(AbstractHttpConfigurer::disable);
+
+        // http basic 인증 방식 disable
+        http
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        http
+                .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class);
+
+        http
+                .addFilterBefore(new JwtExceptionFilter(), JwtAuthenticationFilter.class);
+
+        // 경로별 인가 작업
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(allowedUrls).permitAll()
+                        .requestMatchers("/**").authenticated()
+                        .anyRequest().permitAll()
+                );
+
+        // 예외 처리
+        http
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
+                );
+
+        // 세션 사용 안함
+        http
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        return http.build();
+    }
+}
