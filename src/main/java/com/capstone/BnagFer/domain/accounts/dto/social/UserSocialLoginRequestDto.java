bd77c70d61dd2e70a7ea825b6284ff036e005cd5
gomin0 <gomin0981113@naver.com>
2024-03-14 16:51:48 +0900

    :sparkles: feat: Dto 작성 (#24)

---
+++ src/main/java/com/capstone/BnagFer/domain/accounts/dto/social/UserSocialLoginRequestDto.java
@@ -0,0 +1,6 @@
+package com.capstone.BnagFer.domain.accounts.dto.social;
+
+public record UserSocialLoginRequestDto(
+        String accessToken
+) {
+}
--- src/main/java/com/capstone/BnagFer/domain/accounts/dto/social/UserSocialSignupRequestDto.java
+++ src/main/java/com/capstone/BnagFer/domain/accounts/dto/social/UserSocialSignupRequestDto.java
@@ -5,39 +5,7 @@ import com.capstone.BnagFer.domain.accounts.entity.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 public record UserSocialSignupRequestDto(
-        String email,
-        String password,
-        String name,
-        String nickName,
-        String provider
+        String accessToken,
+        String email
 ) {
-    public User toEntity(PasswordEncoder passwordEncoder) {
-        Profile profile = Profile.builder()
-                .nickname(nickName)
-                .build();
-        User user = User.builder()
-                .email(email)
-                .password(passwordEncoder.encode(password))
-                .name(name)
-                .build();
-        profile.setUser(user);
-        user.setProfile(profile);
-
-        return user;
-    }
-
-    public User toEntity() {
-        Profile profile = Profile.builder()
-                .nickname(nickName)
-                .build();
-        User user = User.builder()
-                .email(email)
-                .name(name)
-                .provider(provider)
-                .build();
-        profile.setUser(user);
-        user.setProfile(profile);
-
-        return user;
-    }
 }
