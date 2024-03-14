34f4e528fd703a06a5d0166b617e715d9b4a9ed6
gomin0 <gomin0981113@naver.com>
2024-03-15 00:47:00 +0900

    :sparkles: feat: User 엔티티 team에 Cascade 조건 추가 (#29)

--- src/main/java/com/capstone/BnagFer/domain/accounts/entity/User.java
+++ src/main/java/com/capstone/BnagFer/domain/accounts/entity/User.java
@@ -53,7 +53,7 @@ public class User extends BaseEntity {
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     private Profile profile;
 
-    @OneToMany(mappedBy = "leader")
+    @OneToMany(mappedBy = "leader", cascade = CascadeType.ALL)
     private List<Team> team;
 
     @OneToMany(mappedBy = "user")
