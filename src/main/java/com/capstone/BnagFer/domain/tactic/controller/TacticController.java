3a36d22ca01569561c2454c48763bfe767749ac4
jiiiiiw <kijiwi1@naver.com>
2024-03-20 16:18:17 +0900

    :recycle: feat: TacticQueryService 코드로 수정 (#31)

--- src/main/java/com/capstone/BnagFer/domain/tactic/controller/TacticController.java
+++ src/main/java/com/capstone/BnagFer/domain/tactic/controller/TacticController.java
@@ -2,6 +2,7 @@ package com.capstone.BnagFer.domain.tactic.controller;
 
 import com.capstone.BnagFer.domain.tactic.dto.TacticRequest;
 import com.capstone.BnagFer.domain.tactic.dto.TacticResponse;
+import com.capstone.BnagFer.domain.tactic.service.TacticQueryService;
 import com.capstone.BnagFer.domain.tactic.service.TacticService;
 import com.capstone.BnagFer.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
@@ -14,16 +15,17 @@ import java.util.List;
 @RequestMapping("/api/v1/tactics")
 public class TacticController {
     private final TacticService tacticService;
+    private final TacticQueryService tacticQueryService;
 
     @GetMapping
     public ApiResponse<List<TacticResponse.TacticList>> getTacticList(){
-        List<TacticResponse.TacticList> tacticLists = tacticService.getTactics();
+        List<TacticResponse.TacticList> tacticLists = tacticQueryService.getTactics();
         return ApiResponse.onSuccess(tacticLists);
     }
 
     @GetMapping("/{tacticId}")
     public ApiResponse<TacticResponse> getTacticDetail(@PathVariable Long tacticId) {
-        TacticResponse tacticDetail = tacticService.getTacticById(tacticId);
+        TacticResponse tacticDetail = tacticQueryService.getTacticById(tacticId);
         return ApiResponse.onSuccess(tacticDetail);
     }
 
@@ -34,8 +36,8 @@ public class TacticController {
     }
 
     @PutMapping("/{tacticId}")
-    public ApiResponse<TacticResponse> updateMyTeam(@PathVariable Long tacticId, String userEmail, @RequestBody TacticRequest.UpdateDTO request) {
-        TacticResponse tacticDetail = tacticService.updateTactic(request);
+    public ApiResponse<TacticResponse> updateMyTeam(@PathVariable Long tacticId, @RequestBody TacticRequest.UpdateDTO request) {
+        TacticResponse tacticDetail = tacticService.updateTactic(tacticId, request);
         return ApiResponse.onSuccess(tacticDetail);
     }
 
