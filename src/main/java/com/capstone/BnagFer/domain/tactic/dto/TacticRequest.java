d4fc8abc409a49cface8738a0e21f3155254da91
jiiiiiw <kijiwi1@naver.com>
2024-03-20 10:36:06 +0900

    :recycle: feat: record를 사용한 dto 리펙토링 (#31)

--- src/main/java/com/capstone/BnagFer/domain/tactic/dto/TacticRequest.java
+++ src/main/java/com/capstone/BnagFer/domain/tactic/dto/TacticRequest.java
@@ -1,6 +1,48 @@
 package com.capstone.BnagFer.domain.tactic.dto;
 
 import com.capstone.BnagFer.domain.tactic.entity.Tactic;
+import jakarta.validation.constraints.NotBlank;
+
+public record TacticRequest() {
+
+    public record CreateDTO(
+            @NotBlank(message = "[ERROR] 전술명은 필수입니다.")
+            String tacticName,
+            @NotBlank(message = "[ERROR] 게시물 공개여부는 필수입니다.")
+            Boolean anonymous,
+            String famousCoachName,
+            @NotBlank(message = "[ERROR] 주 포메이션은 필수입니다.")
+            String mainFormation,
+            @NotBlank(message = "[ERROR] 이메일 입력은 필수입니다.")
+            String tacticDetails,
+            String attackDetails,
+            String defenseDetails
+    ) {
+        public Tactic toEntity() {
+            return Tactic.builder()
+                    .tacticName(tacticName)
+                    .anonymous(anonymous)
+                    .famousCoachName(famousCoachName)
+                    .mainFormation(mainFormation)
+                    .tacticDetails(tacticDetails)
+                    .attackDetails(attackDetails)
+                    .defenseDetails(defenseDetails)
+                    .build();
+        }
+    }
+
+    public record UpdateDTO(
+            String tacticName
+    ) {}
+}
+
+
+
+
+/*
+package com.capstone.BnagFer.domain.tactic.dto;
+
+import com.capstone.BnagFer.domain.tactic.entity.Tactic;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -35,3 +77,4 @@ public class TacticRequest {
     }
 
 }
+*/
