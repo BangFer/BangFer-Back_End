5428427a63874ca0aa2d6a2ef7a29e1aba504461
jiiiiiw <kijiwi1@naver.com>
2024-03-22 13:49:24 +0900

    :bug: feat: Update 시, primary키가 바뀌는 문제 수정 (#37)

--- src/main/java/com/capstone/BnagFer/domain/tactic/service/TacticService.java
+++ src/main/java/com/capstone/BnagFer/domain/tactic/service/TacticService.java
@@ -1,10 +1,10 @@
 package com.capstone.BnagFer.domain.tactic.service;
 
 import com.capstone.BnagFer.domain.accounts.entity.User;
-import com.capstone.BnagFer.domain.accounts.repository.UserJpaRepository;
 import com.capstone.BnagFer.domain.accounts.service.AccountsServiceUtils;
-import com.capstone.BnagFer.domain.tactic.dto.TacticRequest;
+import com.capstone.BnagFer.domain.tactic.dto.TacticCreateRequest;
 import com.capstone.BnagFer.domain.tactic.dto.TacticResponse;
+import com.capstone.BnagFer.domain.tactic.dto.TacticUpdateRequest;
 import com.capstone.BnagFer.domain.tactic.entity.Tactic;
 import com.capstone.BnagFer.domain.tactic.exception.TacticExceptionHandler;
 import com.capstone.BnagFer.domain.tactic.repository.TacticRepository;
@@ -22,7 +22,7 @@ public class TacticService {
     private final TacticRepository tacticRepository;
     private final AccountsServiceUtils accountsServiceUtils;
 
-    public TacticResponse createTactic(TacticRequest.CreateDTO request){
+    public TacticResponse createTactic(TacticCreateRequest request){
         User user = accountsServiceUtils.getCurrentUser();
         Tactic tactic = request.toEntity(user);
         tacticRepository.save(tactic);
@@ -39,14 +39,23 @@ public class TacticService {
         tacticRepository.deleteById(tacticId);
     }
 
-    public TacticResponse updateTactic(Long tacticId, TacticRequest.UpdateDTO request) {
+    public TacticResponse updateTactic(Long tacticId, TacticUpdateRequest request) {
         User user = accountsServiceUtils.getCurrentUser();
         Tactic tactic = tacticRepository.findById(tacticId).orElseThrow(() -> new TacticExceptionHandler(ErrorCode.TACTIC_NOT_FOUND));
 
         if(tactic.getUser() != user)
             throw new TacticExceptionHandler(ErrorCode.USER_NOT_MATCHED);
 
-        tactic.updateTactic(request);
+        tactic.setTacticName(request.tacticName());
+        tactic.setUser(user);
+        tactic.setAnonymous(request.anonymous());
+        tactic.setFamousCoachName(request.famousCoachName());
+        tactic.setMainFormation(request.mainFormation());
+        tactic.setAttackFormation(request.attackFormation());
+        tactic.setDefenseFormation(request.defenseFormation());
+        tactic.setTacticDetails(request.tacticDetails());
+        tactic.setAttackDetails(request.attackDetails());
+        tactic.setAttackDetails(request.attackDetails());
         Tactic updatedTactic = tacticRepository.save(tactic);
         return TacticResponse.from(updatedTactic);
     }
