eafc043e980f33e6b075284d1d158ea7f0267188
600gramSik <geunsikevin@naver.com>
2024-03-31 22:40:25 +0900

    ✨feature:팀원 조회 구현 (#50)

--- src/main/java/com/capstone/BnagFer/domain/myteam/service/TeamMembersQueryService.java
+++ src/main/java/com/capstone/BnagFer/domain/myteam/service/TeamMembersQueryService.java
@@ -1,4 +1,36 @@
 package com.capstone.BnagFer.domain.myteam.service;
 
+import com.capstone.BnagFer.domain.accounts.repository.UserJpaRepository;
+import com.capstone.BnagFer.domain.accounts.service.AccountsServiceUtils;
+import com.capstone.BnagFer.domain.myteam.dto.TeamMembersResponseDto;
+import com.capstone.BnagFer.domain.myteam.entity.Team;
+import com.capstone.BnagFer.domain.myteam.entity.TeamMember;
+import com.capstone.BnagFer.domain.myteam.exception.TeamExceptionHandler;
+import com.capstone.BnagFer.domain.myteam.repository.TeamMembersRepository;
+import com.capstone.BnagFer.domain.myteam.repository.TeamRepository;
+import com.capstone.BnagFer.global.common.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class TeamMembersQueryService {
+    private final AccountsServiceUtils accountsServiceUtils;
+    private final TeamRepository teamRepository;
+    private final TeamMembersRepository teamMembersRepository;
+    private final UserJpaRepository userJpaRepository;
+
+    public List<TeamMembersResponseDto> getMembers(Long teamId) {
+        Team team = teamRepository.findById(teamId).orElseThrow(() -> new TeamExceptionHandler(ErrorCode.TEAM_NOT_FOUND));
+         List<TeamMember> teamMembers = teamMembersRepository.findByTeam(team);
+        List<TeamMembersResponseDto> teamMemberResponseDtos = teamMembers.stream()
+                .map(TeamMembersResponseDto::from)
+                .toList();
+        return teamMemberResponseDtos;
+    }
 }
