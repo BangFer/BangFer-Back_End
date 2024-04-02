9b73561f22a2788c4f1afdd41df94f249388d80c
gomin0 <gomin0981113@naver.com>
2024-04-02 23:25:07 +0900

    :lipstick: style: 사용되지 않는 import 문 제거 (#48)

--- src/main/java/com/capstone/BnagFer/domain/accounts/dto/profile/CreateProfileRequestDto.java
+++ src/main/java/com/capstone/BnagFer/domain/accounts/dto/profile/CreateProfileRequestDto.java
@@ -3,8 +3,6 @@ package com.capstone.BnagFer.domain.accounts.dto.profile;
 import com.capstone.BnagFer.domain.accounts.entity.Gender;
 import com.capstone.BnagFer.domain.accounts.entity.Profile;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Past;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
