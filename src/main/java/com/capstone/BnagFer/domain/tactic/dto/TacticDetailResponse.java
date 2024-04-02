a25a379e8ba0105565149d8709c4e8eebc54f1d3
jiiiiiw <kijiwi1@naver.com>
2024-04-03 01:32:43 +0900

    :recycle: feat: 전술 게시물 개별조회시 사용하는 TacticDetailRes DTO 분리하여 작성 (#61)

---
+++ src/main/java/com/capstone/BnagFer/domain/tactic/dto/TacticDetailResponse.java
@@ -0,0 +1,69 @@
+package com.capstone.BnagFer.domain.tactic.dto;
+
+import com.capstone.BnagFer.domain.tactic.entity.Tactic;
+import com.capstone.BnagFer.domain.tactic.entity.TacticComment;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Builder
+public record TacticDetailResponse(
+        Long tacticId,
+        Long userId,
+        String tacticName,
+        Boolean anonymous,
+        String famousCoachName,
+        String mainFormation,
+        byte[] attackFormation,
+        byte[] defenseFormation,
+        String tacticDetails,
+        String attackDetails,
+        String defenseDetails,
+        List<CommentList> comments,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {
+    public static TacticDetailResponse from(Tactic tactic) {
+        return TacticDetailResponse.builder()
+                .tacticId(tactic.getTacticId())
+                .userId(tactic.getUser().getId())
+                .tacticName(tactic.getTacticName())
+                .anonymous(tactic.isAnonymous())
+                .famousCoachName(tactic.getFamousCoachName())
+                .mainFormation(tactic.getMainFormation())
+                .attackFormation(tactic.getAttackFormation())
+                .defenseFormation(tactic.getDefenseFormation())
+                .tacticDetails(tactic.getTacticDetails())
+                .attackDetails(tactic.getAttackDetails())
+                .defenseDetails(tactic.getDefenseDetails())
+                .comments(CommentList.from(tactic.getComments()))
+                .createdAt(tactic.getCreatedAt())
+                .updatedAt(tactic.getUpdatedAt())
+                .build();
+    }
+    @Builder
+    public record CommentList(
+            Long tacticCommentId,
+            Long tacticId,
+            Long userId,
+            String comment,
+            LocalDateTime createdAt,
+            LocalDateTime updateAt
+    ){
+        public static CommentList from(TacticComment comment){
+            return CommentList.builder()
+                    .tacticCommentId(comment.getCommentId())
+                    .tacticId(comment.getTactic().getTacticId())
+                    .userId(comment.getUser().getId())
+                    .comment(comment.getComment())
+                    .createdAt(comment.getCreatedAt())
+                    .updateAt(comment.getUpdatedAt())
+                    .build();
+        }
+        public static List<CommentList> from(List<TacticComment> comments){
+            return comments.stream().map(CommentList::from).collect(Collectors.toList());
+        }
+    }
+}
