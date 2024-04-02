f0a6f1cbf16d9043efb129c00e26270de873e6f5
gomin0 <gomin0981113@naver.com>
2024-04-02 23:22:15 +0900

    :sparkles: feat: ProfileResponseDto 작성 (#48)

---
+++ src/main/java/com/capstone/BnagFer/domain/accounts/dto/profile/ProfileResponseDto.java
@@ -0,0 +1,34 @@
+package com.capstone.BnagFer.domain.accounts.dto.profile;
+
+import com.capstone.BnagFer.domain.accounts.entity.Gender;
+import com.capstone.BnagFer.domain.accounts.entity.Profile;
+import com.capstone.BnagFer.domain.accounts.entity.User;
+import lombok.Builder;
+
+import java.time.LocalDate;
+
+@Builder
+public record ProfileResponseDto(
+        Long userId,
+        Long profileId,
+        String name,
+        String nickName,
+        String email,
+        String description,
+        LocalDate dateOfBirth,
+        Gender gender
+) {
+    public static ProfileResponseDto from(Profile profile, User user) {
+        return ProfileResponseDto.builder()
+                .userId(user.getId())
+                .profileId(profile.getId())
+                .name(user.getName())
+                .nickName(profile.getNickname())
+                .email(user.getEmail())
+                .description(profile.getDescription())
+                .description(profile.getDescription())
+                .dateOfBirth(profile.getDateOfBirth())
+                .gender(profile.getGender())
+                .build();
+    }
+}
