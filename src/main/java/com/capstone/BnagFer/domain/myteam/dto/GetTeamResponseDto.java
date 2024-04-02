f3a7d59b5a237afc56e8c6178b31b8a66a6a61a0
600gramSik <geunsikevin@naver.com>
2024-04-02 14:10:23 +0900

    ♻️refactor:팀 조회 시 팀원, 적용된 전술 추가 (#50)

--- src/main/java/com/capstone/BnagFer/domain/myteam/dto/GetTeamResponseDto.java
+++ src/main/java/com/capstone/BnagFer/domain/myteam/dto/GetTeamResponseDto.java
@@ -1,11 +1,18 @@
 package com.capstone.BnagFer.domain.myteam.dto;
+import com.capstone.BnagFer.domain.myteam.entity.Role;
 import com.capstone.BnagFer.domain.myteam.entity.Team;
 import com.capstone.BnagFer.domain.myteam.entity.TeamMember;
 import com.capstone.BnagFer.domain.tactic.dto.TacticResponse;
+import com.capstone.BnagFer.domain.tactic.entity.Position;
 import com.capstone.BnagFer.domain.tactic.entity.Tactic;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Builder
 public record GetTeamResponseDto (
         Long id,
@@ -13,20 +20,47 @@ public record GetTeamResponseDto (
         String leaderName, //leader_name
         String teamName,
         TacticResponse tacticDto,
-        List <TeamMember> teamMembers,
+        List<TeamMembersList> teamMembers,
         LocalDateTime createdAt
 ) {
+
+
     //DTO에서 다른 객체가 참조되면 null값으로 넣어두고 Service단에서 처리해주는 식으로~
-    public static GetTeamResponseDto from(Team team) {
+    public static GetTeamResponseDto from(Long teamId, Long leaderId, String leaderName, String teamName, List<TeamMembersList> teamMembers, Tactic tacticDto, LocalDateTime createdAt) {
         return GetTeamResponseDto.builder()
-                .id(team.getId())
-                .leaderId(team.getLeader().getId()) //null로 넣어두고 service단에서 id값 추가해주기
-                .leaderName(team.getLeader().getName())
-                .teamName(team.getTeamName())
-//                .tacticDto(TacticResponse.from(tactic))
-                .teamMembers(null)
-                .createdAt(team.getCreatedAt())
+                .id(teamId)
+                .leaderId(leaderId) //null로 넣어두고 service단에서 id값 추가해주기
+                .leaderName(leaderName)
+                .teamName(teamName)
+                .teamMembers(teamMembers)
+                .tacticDto(TacticResponse.from(tacticDto))
+                .createdAt(createdAt)
                 .build();
     }
+
+
+    @Builder
+    public record TeamMembersList(
+            Long userId,
+            Role role,
+            Position position
+
+    ) {
+        public static TeamMembersList from(TeamMember teamMember) {
+            return TeamMembersList.builder()
+                    .userId(teamMember.getUser().getId())
+                    .role(teamMember.getRole())
+                    .position(teamMember.getPosition())
+                    .build();
+        }
+        public static List<TeamMembersList> from(List<TeamMember> teamMembers) {
+            return teamMembers.stream().map(TeamMembersList::from).collect(Collectors.toList());
+        }
+
+
+
+
+    }
 }
+
 
