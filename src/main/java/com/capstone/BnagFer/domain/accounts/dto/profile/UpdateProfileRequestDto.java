b45c2f1510f8a07104403f73c9039a27c5913eab
gomin0 <gomin0981113@naver.com>
2024-04-02 23:20:41 +0900

    :sparkles: feat: UpdateProfileRequestDto 작성 (#48)

---
+++ src/main/java/com/capstone/BnagFer/domain/accounts/dto/profile/UpdateProfileRequestDto.java
@@ -0,0 +1,32 @@
+package com.capstone.BnagFer.domain.accounts.dto.profile;
+
+import com.capstone.BnagFer.domain.accounts.entity.Gender;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+
+import java.time.LocalDate;
+
+@Builder
+public record UpdateProfileRequestDto(
+
+        @NotBlank(message = "닉네임은 필수입니다.")
+        @Size(max = 10, message = "닉네임은 최대 10자까지 입력 가능합니다.")
+        String nickname,
+
+        @NotBlank(message = "[ERROR] 이름 입력은 필수 입니다.")
+        @Size(max = 10, message = "이름은 최대 10자까지 입력 가능합니다.")
+        String name,
+
+        String description,
+
+        @NotBlank(message = "[ERROR] 이메일 입력은 필수입니다.")
+        @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "[ERROR] 이메일 형식에 맞지 않습니다.")
+        String email,
+
+        LocalDate dateOfBirth,
+
+        Gender gender
+) {
+}
