40883e5462df5046beb603a05bb844996384e425
jiiiiiw <kijiwi1@naver.com>
2024-05-02 16:33:15 +0900

    :recycle: refactor: TacticController에 주석 설명 (#85)

--- src/main/java/com/capstone/BnagFer/domain/tactic/controller/TacticController.java
+++ src/main/java/com/capstone/BnagFer/domain/tactic/controller/TacticController.java
@@ -37,6 +37,7 @@ public class TacticController {
         return ApiResponse.onSuccess(userTacticLists);
     }
 
+    // 전술 게시물 생성
     @PostMapping
     public ApiResponse<TacticResponse> createTactic(@RequestBody TacticCreateRequest request){
         TacticResponse tacticDetail = tacticService.createTactic(request);
@@ -57,30 +58,35 @@ public class TacticController {
         return ApiResponse.onSuccess(tacticDetail);
     }
 
+    // 전술 게시물 삭제
     @DeleteMapping("/{tacticId}")
     public ApiResponse<Object> deleteTactic(@PathVariable Long tacticId){
         tacticService.deleteTactic(tacticId);
         return ApiResponse.noContent();
     }
 
+    // 전술 게시물에 댓글 달기
     @PostMapping("/{tacticId}/comment")
     public ApiResponse<CommentResponse> createComment(@PathVariable Long tacticId, @RequestBody CommentCreateRequest request) {
         CommentResponse commentDetail = tacticService.createComment(tacticId, request);
         return ApiResponse.onSuccess(commentDetail);
     }
 
+    // 전술 게시물 댓글 조희
     @PutMapping("/comment/{commentId}")
     public ApiResponse<CommentResponse> updateComment(@PathVariable Long commentId, @RequestBody CommentUpdateRequest request) {
         CommentResponse commentDetail = tacticService.updateComment(commentId, request);
         return ApiResponse.onSuccess(commentDetail);
     }
 
+    // 댓글 삭제
     @DeleteMapping("/comment/{commentId}")
     public ApiResponse<Object> deleteComment(@PathVariable Long commentId){
         tacticService.deleteComment(commentId);
         return ApiResponse.noContent();
     }
 
+    // 좋아요 누르기 및 취소하기
     @PostMapping("/{tacticId}/like")
     public ApiResponse<Object> likeToggle(@PathVariable Long tacticId) {
         return tacticService.likeButton(tacticId);
