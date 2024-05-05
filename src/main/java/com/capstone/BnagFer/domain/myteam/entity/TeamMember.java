33c3ef3c0e60b44bc4572ccd924b3f55dfaa282a
gomin0 <gomin0981113@naver.com>
2024-05-06 01:51:22 +0900

    [REFACTOR] Team 엔티티 관련 리팩토링(#93) (#94)
    * :recycle: refactor: 매서드 추가 (#93)
    
    * :recycle: refactor: Dto 수정 (#93)
    
    * :recycle: refactor: 공통 매서드 뽑기 (#93)
    
    * :recycle: refactor: 서비스 수정 (#93)
    
    * :recycle: refactor: 사용되지 않는 import문, 변수 삭제, 인라인화 (#93)
    
    * :recycle: refactor: 사용되지 않는 import문, 변수 삭제, 인라인화 (#93)

--- src/main/java/com/capstone/BnagFer/domain/myteam/entity/TeamMember.java
+++ src/main/java/com/capstone/BnagFer/domain/myteam/entity/TeamMember.java
@@ -32,19 +32,17 @@ import lombok.*;
     @Column(name = "position")
     private Position position;
 
-    public void setUser(User user) {
-        this.user = user;
-    }
+   public void updateRole(Role upateRole) {
+      role = upateRole;
+   }
 
-    public void setRole(Role role) {
-        this.role = role;
-    }
+   public void updateUserRoleAndTeam(User updateUser, Team updateTeam) {
+      user = updateUser;
+      role = Role.LEADER;
+      team  = updateTeam;
+   }
 
-    public void setTeam(Team team) {
-        this.team = team;
-    }
-
-    public void setPosition(Position position) { this.position = position; }
+   public void updatePosition(Position updatePosition) { position = updatePosition; }
 
     public static TeamMember createTeamMember() {
         return new TeamMember();
--- src/main/java/com/capstone/BnagFer/domain/myteam/service/TeamService.java
+++ src/main/java/com/capstone/BnagFer/domain/myteam/service/TeamService.java
@@ -4,7 +4,6 @@ import com.capstone.BnagFer.domain.accounts.entity.User;
 import com.capstone.BnagFer.domain.accounts.service.AccountsServiceUtils;
 import com.capstone.BnagFer.domain.myteam.dto.CUTeamRequestDto;
 import com.capstone.BnagFer.domain.myteam.dto.CUTeamResponseDto;
-import com.capstone.BnagFer.domain.myteam.entity.Role;
 import com.capstone.BnagFer.domain.myteam.entity.Team;
 import com.capstone.BnagFer.domain.myteam.entity.TeamMember;
 import com.capstone.BnagFer.domain.myteam.exception.TeamExceptionHandler;
@@ -22,19 +21,18 @@ public class TeamService {
     private final TeamRepository teamRepository;
     private final AccountsServiceUtils accountsServiceUtils;
     private final TeamMembersRepository teamMembersRepository;
+    private final TeamServiceUtils teamServiceUtils;
 
     public CUTeamResponseDto createMyTeam(CUTeamRequestDto request) {
         User user = accountsServiceUtils.getCurrentUser();
         Team team = request.toEntity();
-        team.setLeader(user);
+        team.updateLeader(user);
         TeamMember teamMember = TeamMember.createTeamMember();
 
         // 프로필 존재 확인
         accountsServiceUtils.checkUserProfile(team.getLeader());
 
-        teamMember.setTeam(team);
-        teamMember.setUser(user);
-        teamMember.setRole(Role.LEADER);
+        teamMember.updateUserRoleAndTeam(user, team);
         teamMembersRepository.save(teamMember);
         teamRepository.save(team);
         return CUTeamResponseDto.from(team);
@@ -42,8 +40,9 @@ public class TeamService {
 
     public CUTeamResponseDto updateMyTeam(CUTeamRequestDto request, Long teamId) {
         User user = accountsServiceUtils.getCurrentUser();
-        Team team = teamRepository.findById(teamId).orElseThrow(() -> new TeamExceptionHandler(ErrorCode.TEAM_NOT_FOUND));
-        if(team.getLeader().getId() != user.getId()) {
+        Team team = teamServiceUtils.checkValidTeam(teamId);
+        //방장만 팀의 업데이트를 할 수 있다
+        if(!team.getLeader().getId().equals(user.getId())) {
             throw new TeamExceptionHandler(ErrorCode.USER_NOT_MATCHED);
         }
         team.updateTeam(request);
@@ -52,10 +51,12 @@ public class TeamService {
     }
     public void deleteMyTeam(Long teamId) {
         User user = accountsServiceUtils.getCurrentUser();
-        Team team = teamRepository.findById(teamId).orElseThrow(() ->new TeamExceptionHandler(ErrorCode.TEAM_NOT_FOUND));
+        Team team = teamServiceUtils.checkValidTeam(teamId);
 
-        if(team.getLeader().getId()!= user.getId()) {
+        //방장만이 강퇴 가능
+        if (!team.getLeader().getId().equals(user.getId())) {
             throw new TeamExceptionHandler(ErrorCode.USER_NOT_MATCHED);
         }
-        teamRepository.deleteById(teamId); }
+        teamRepository.deleteById(teamId);
+    }
 }
