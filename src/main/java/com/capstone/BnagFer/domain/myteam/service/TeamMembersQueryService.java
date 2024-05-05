bbccb1eccfd65bcc3db52b0b8179c1badd9c1471
Geunsik Kim <93639883+600gramSik@users.noreply.github.com>
2024-05-06 00:38:32 +0900

    Revert "[REFACTOR] Team 엔티티 관련 리팩토링(#87)"

--- src/main/java/com/capstone/BnagFer/domain/myteam/service/TeamMembersQueryService.java
+++ src/main/java/com/capstone/BnagFer/domain/myteam/service/TeamMembersQueryService.java
@@ -21,12 +21,13 @@ import java.util.List;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class TeamMembersQueryService {
+    private final AccountsServiceUtils accountsServiceUtils;
     private final TeamRepository teamRepository;
     private final TeamMembersRepository teamMembersRepository;
-    private final TeamServiceUtils teamServiceUtils;
+    private final UserJpaRepository userJpaRepository;
 
     public List<TeamMembersResponseDto> getMembers(Long teamId) {
-        Team team = teamServiceUtils.checkValidTeam(teamId);
+        Team team = teamRepository.findById(teamId).orElseThrow(() -> new TeamExceptionHandler(ErrorCode.TEAM_NOT_FOUND));
         List<TeamMember> teamMembers = teamMembersRepository.findByTeam(team);
         if(teamMembers.isEmpty()) {
             throw new TeamMemberExceptionHandler(ErrorCode.CANNOT_FIND_TEAMMEMBER);
