249075b76a5e072066c521bda87f6c43a069df7c
gomin0 <gomin0981113@naver.com>
2024-05-06 01:39:56 +0900

    :recycle: refactor: 공통 매서드 뽑기 (#93)

---
+++ src/main/java/com/capstone/BnagFer/domain/myteam/service/TeamServiceUtils.java
@@ -0,0 +1,23 @@
+package com.capstone.BnagFer.domain.myteam.service;
+
+import com.capstone.BnagFer.domain.accounts.entity.User;
+import com.capstone.BnagFer.domain.myteam.entity.Team;
+import com.capstone.BnagFer.domain.myteam.exception.TeamExceptionHandler;
+import com.capstone.BnagFer.domain.myteam.exception.TeamMemberExceptionHandler;
+import com.capstone.BnagFer.domain.myteam.repository.TeamRepository;
+import com.capstone.BnagFer.global.common.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly=true)
+@RequiredArgsConstructor
+public class TeamServiceUtils {
+    private final TeamRepository teamRepository;
+
+    public Team checkValidTeam(Long teamId) {
+        Team team = teamRepository.findById(teamId).orElseThrow(() ->new TeamExceptionHandler(ErrorCode.TEAM_NOT_FOUND));
+        return team;
+    }
+}
