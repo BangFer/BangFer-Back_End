e590d54fbb87e1423f63b5aa7ca67078a4562047
gomin0 <gomin0981113@naver.com>
2024-05-12 19:47:28 +0900

    :recycle: refactor: 어노테이션 수정 (#99)

--- src/main/java/com/capstone/BnagFer/global/annotation/LoginUserArgumentResolver.java
+++ src/main/java/com/capstone/BnagFer/global/annotation/LoginUserArgumentResolver.java
@@ -3,11 +3,15 @@ package com.capstone.BnagFer.global.annotation;
 import com.capstone.BnagFer.domain.accounts.entity.User;
 import com.capstone.BnagFer.domain.accounts.jwt.exception.SecurityCustomException;
 import com.capstone.BnagFer.domain.accounts.jwt.exception.TokenErrorCode;
+import com.capstone.BnagFer.domain.accounts.jwt.userdetails.CustomUserDetails;
 import com.capstone.BnagFer.domain.accounts.jwt.util.JwtProvider;
 import com.capstone.BnagFer.domain.accounts.repository.UserJpaRepository;
+import com.capstone.BnagFer.domain.accounts.service.AccountsQueryService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -15,6 +19,7 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 @Transactional
@@ -22,6 +27,7 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
 
     private final JwtProvider jwtProvider;
     private final UserJpaRepository userJpaRepository;
+    private final AccountsQueryService accountsQueryService;
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -34,11 +40,11 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
     public Object resolveArgument(MethodParameter parameter,
                                   ModelAndViewContainer mavContainer,
                                   NativeWebRequest webRequest,
-                                  WebDataBinderFactory binderFactory) throws Exception {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) webRequest.getNativeRequest();
-        String token = jwtProvider.resolveAccessToken(httpServletRequest);
-        String email = jwtProvider.getUserEmail(token);
-        return userJpaRepository.findByEmail(email)
-                .orElseThrow(() -> new SecurityCustomException(TokenErrorCode.TOKEN_NOT_FOUND));
+                                  WebDataBinderFactory binderFactory) {
+        Object userDetails = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        log.info("User details from security context: {}", userDetails);
+        User user = accountsQueryService.getUserByEmail(((CustomUserDetails)userDetails).getUsername());
+        log.info("User details from accountsQueryService: {}", user);
+        return user;
     }
 }
