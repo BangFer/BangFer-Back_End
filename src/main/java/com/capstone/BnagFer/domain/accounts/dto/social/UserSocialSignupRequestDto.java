484f04c29f85d5bb0ce9ac303953cdadc5b25dc9
gomin0 <gomin0981113@naver.com>
2024-05-14 11:02:42 +0900

    :lipstick: style: import문 삭제 (#104)

--- src/main/java/com/capstone/BnagFer/domain/accounts/dto/social/UserSocialSignupRequestDto.java
+++ src/main/java/com/capstone/BnagFer/domain/accounts/dto/social/UserSocialSignupRequestDto.java
@@ -1,9 +1,6 @@
 package com.capstone.BnagFer.domain.accounts.dto.social;
 
-import com.capstone.BnagFer.domain.accounts.entity.Profile;
-import com.capstone.BnagFer.domain.accounts.entity.User;
 import jakarta.validation.constraints.NotBlank;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 public record UserSocialSignupRequestDto(
         @NotBlank(message = "[ERROR] 토큰 입력은 필수 입니다.")
