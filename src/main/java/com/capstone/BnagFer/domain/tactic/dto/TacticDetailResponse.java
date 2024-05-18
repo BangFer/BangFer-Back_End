18968ed8ec97f32f1ff85a07b4a37f550fea6826
jiiiiiw <kijiwi1@naver.com>
2024-05-18 15:32:27 +0900

    :recycle: refactor: 11명 포지션별 개별 조회로 인한 삭제 (#100)

--- src/main/java/com/capstone/BnagFer/domain/tactic/dto/TacticDetailResponse.java
+++ src/main/java/com/capstone/BnagFer/domain/tactic/dto/TacticDetailResponse.java
@@ -25,7 +25,6 @@ public record TacticDetailResponse(
         String attackDetails,
         String defenseDetails,
         List<CommentList> comments,
-        List<DetailList> detailpositions,
         int likeCnt,
         LocalDateTime createdAt,
         LocalDateTime updatedAt
@@ -45,7 +44,6 @@ public record TacticDetailResponse(
                 .attackDetails(tactic.getAttackDetails())
                 .defenseDetails(tactic.getDefenseDetails())
                 .comments(CommentList.from(tactic.getComments()))
-                .detailpositions(DetailList.from(tactic.getTacticPositionDetails()))
                 .likeCnt(tactic.getLikes().size())
                 .createdAt(tactic.getCreatedAt())
                 .updatedAt(tactic.getUpdatedAt())
@@ -74,26 +72,6 @@ public record TacticDetailResponse(
         }
         public static List<CommentList> from(List<TacticComment> comments){
             return comments.stream().map(CommentList::from).collect(Collectors.toList());
-        }
-    }
-
-    @Builder
-    public record DetailList(
-            Long detailId,
-            Long tacticId,
-            Position position,
-            String positionDescription
-    ){
-        public static DetailList from(TacticPositionDetail tacticPositionDetail){
-            return DetailList.builder()
-                    .detailId(tacticPositionDetail.getDetailId())
-                    .tacticId(tacticPositionDetail.getTactic().getTacticId())
-                    .position(tacticPositionDetail.getPosition())
-                    .positionDescription(tacticPositionDetail.getPositionDescription())
-                    .build();
-        }
-        public static List<DetailList> from(List<TacticPositionDetail> details){
-            return details.stream().map(DetailList::from).collect(Collectors.toList());
         }
     }
 }
