bd5e6a81eaf3289e821f8ef1d9cee2e90e71bb84
gomin0 <gomin0981113@naver.com>
2024-05-26 10:20:20 +0900

    :recycle: refactor: stringbuilder 사용 (#118)

--- src/main/java/com/capstone/BnagFer/domain/accounts/dto/email/EmailVerifyDto.java
+++ src/main/java/com/capstone/BnagFer/domain/accounts/dto/email/EmailVerifyDto.java
@@ -2,8 +2,6 @@ package com.capstone.BnagFer.domain.accounts.dto.email;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 
 public record EmailVerifyDto(
         @NotBlank(message = "[ERROR] 이메일 입력은 필수 입니다.")
