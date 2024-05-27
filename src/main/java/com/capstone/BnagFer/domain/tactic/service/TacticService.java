240074b626ccea9db9583d2195aaf7bb2141c341
gomin0 <gomin0981113@naver.com>
2024-05-28 00:53:16 +0900

    :bug: fix: 좋아요 개수 redis 사용 버그 수정 (#120)

--- src/main/java/com/capstone/BnagFer/domain/tactic/service/TacticService.java
+++ src/main/java/com/capstone/BnagFer/domain/tactic/service/TacticService.java
@@ -136,53 +136,23 @@ public class TacticService {
         commentRepository.deleteById(commentId);
     }
 
-    /*public ApiResponse<Object> likeButton(Long tacticId, User user) {
-        String key = "tactic:" + tacticId + ":likes";
-        ValueOperations<String, String> valueOps = redisTemplate.opsForValue();
-
-        Tactic tactic = tacticRepository.findById(tacticId).orElseThrow(() -> new TacticExceptionHandler(ErrorCode.TACTIC_NOT_FOUND));
-        Optional<TacticLike> like = likeRepository.findByUserAndTactic(user, tactic);
-
-        if (like.isPresent()) {
-            likeRepository.delete(like.get());
-            valueOps.increment(key, -1);
-            return ApiResponse.CANCELED_LIKE();
-        }
-        else {
-            likeRepository.save(new TacticLike(user, tactic));
-            valueOps.increment(key, 1);
-            return ApiResponse.SUCCESS_LIKE();
-        }
-    }
-
-    public Long getLikeCount(Long tacticId) {
-        String key = "tactic:" + tacticId + ":likes";
-        ValueOperations<String, String> valueOps = redisTemplate.opsForValue();
-        String likeCount = valueOps.get(key);
-        return likeCount != null ? Long.parseLong(likeCount) : 0;
-    }*/
-
     public ApiResponse<Object> likeButton(Long tacticId, User user) {
 
         Tactic tactic = tacticRepository.findById(tacticId).orElseThrow(() -> new TacticExceptionHandler(ErrorCode.TACTIC_NOT_FOUND));
 
         Optional<TacticLike> like = likeRepository.findByUserAndTactic(user, tactic);
-        String redisKey = "tactic:likes:" + tacticId;
 
-        Long currentLikes = redisUtil.getLikes(redisKey);
-        if (currentLikes == null) {
-            currentLikes = (long) tactic.getLikes().size();
-            redisUtil.save(redisKey, currentLikes, 30L, TimeUnit.DAYS);
-        }
+        long likeCount = redisUtil.getLikeCount(tacticId);;
 
         if (like.isPresent()) {
             likeRepository.delete(like.get());
-            redisUtil.save(redisKey, currentLikes - 1, 30L, TimeUnit.DAYS);
+            likeCount--;
+            redisUtil.saveLikeCount(tacticId, likeCount);
             return ApiResponse.CANCELED_LIKE();
-        }
-        else {
+        } else {
             likeRepository.save(new TacticLike(user, tactic));
-            redisUtil.save(redisKey, currentLikes + 1, 30L, TimeUnit.DAYS);
+            likeCount++;
+            redisUtil.saveLikeCount(tacticId, likeCount);
             return ApiResponse.SUCCESS_LIKE();
         }
     }
