d5ed2fa7832335db5c10d62db46e02cfee6317ac
jiiiiiw <kijiwi1@naver.com>
2024-05-29 02:22:35 +0900

    :sparkles: feat: 정적 리소스 경로에 대한 접근 허용 (#122)

--- src/main/java/com/capstone/BnagFer/global/config/SecurityConfig.java
+++ src/main/java/com/capstone/BnagFer/global/config/SecurityConfig.java
@@ -32,8 +32,11 @@ public class SecurityConfig {
     private final String[] swaggerUrls = {"/swagger-ui/**", "/v3/**"};
     private final String[] authUrls = {"/", "/accounts/signup/**", "/accounts/social/**", "/accounts/login/**",
             "/api/v1/auth", "/oauth/kakao/**", "/accounts/reissue/**", "/accounts/forgotPw/**", "/accounts/email/send-email", "/accounts/email/verify"};
-    private final String[] allowedUrls = Stream.concat(Arrays.stream(swaggerUrls), Arrays.stream(authUrls))
-            .toArray(String[]::new);
+    private final String[] staticResourceUrls = {"/images/**"};
+    private final String[] allowedUrls = Stream.concat(
+            Stream.concat(Arrays.stream(swaggerUrls), Arrays.stream(authUrls)),
+            Arrays.stream(staticResourceUrls)
+    ).toArray(String[]::new);
 
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
