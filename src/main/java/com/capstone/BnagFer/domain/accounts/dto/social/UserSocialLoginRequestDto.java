1aae38961efe973e0a97049d961c71a4e63f4e7d
gomin0 <gomin0981113@naver.com>
2024-06-27 11:56:43 +0900

    :lipstick: style: import문 삭제 (#135)

--- src/main/java/com/capstone/BnagFer/domain/accounts/dto/social/UserSocialLoginRequestDto.java
+++ src/main/java/com/capstone/BnagFer/domain/accounts/dto/social/UserSocialLoginRequestDto.java
@@ -1,6 +1,5 @@
 package com.capstone.BnagFer.domain.accounts.dto.social;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record UserSocialLoginRequestDto(
