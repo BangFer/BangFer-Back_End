40ff48e0e0d5dc2a84efe197ad6f98d58f370481
gomin0 <gomin0981113@naver.com>
2024-07-02 16:15:03 +0900

    :recycle: refactor: 개별 전술 생성, 수정 로직 수정 (#137)

--- src/main/java/com/capstone/BnagFer/domain/tactic/repository/TacticPositionDetailRepository.java
+++ src/main/java/com/capstone/BnagFer/domain/tactic/repository/TacticPositionDetailRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface TacticPositionDetailRepository extends JpaRepository<TacticPositionDetail, Long> {
-    @Transactional
     void deleteByTactic_TacticId(Long tacticId);
 }
