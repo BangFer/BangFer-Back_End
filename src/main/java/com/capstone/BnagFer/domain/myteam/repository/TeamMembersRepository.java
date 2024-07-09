71af00ed3986947e4628757c4c6581e295b5c0da
Geunsik Kim <geunsikevin@naver.com>
2024-07-09 16:45:52 +0900

    ✨FEATURE: 팀원 강퇴 기능에 매개변수로 teamId값 추가해서 특정 팀에서 강퇴할 수 있게 구현 (#126)

--- src/main/java/com/capstone/BnagFer/domain/myteam/repository/TeamMembersRepository.java
+++ src/main/java/com/capstone/BnagFer/domain/myteam/repository/TeamMembersRepository.java
@@ -4,14 +4,11 @@ import com.capstone.BnagFer.domain.accounts.entity.User;
 import com.capstone.BnagFer.domain.myteam.entity.Team;
 import com.capstone.BnagFer.domain.myteam.entity.TeamMember;
 import com.capstone.BnagFer.domain.tactic.entity.Position;
-import com.capstone.BnagFer.domain.tactic.entity.TacticPositionDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
-import java.util.Optional;
 @Repository
 public interface TeamMembersRepository extends JpaRepository<TeamMember, Long> {
     @Query("select m from TeamMember m " +
