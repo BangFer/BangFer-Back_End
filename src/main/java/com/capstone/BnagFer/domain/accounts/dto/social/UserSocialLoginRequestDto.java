c939da8897725563cff2e310d38bc6218eb2f439
gomin0 <gomin0981113@naver.com>
2024-07-09 16:04:57 +0900

    [FEATURE] 카카오 프로필에서 이메일 가져오기 (#135) (#136)
    * :recycle: refactor: 카카오 회원가입, 로그인 시 카카오에서 이메일 받아오기 (#135)
    
    * :lipstick: style: import문 삭제 (#135)

--- src/main/java/com/capstone/BnagFer/domain/accounts/dto/social/UserSocialLoginRequestDto.java
+++ src/main/java/com/capstone/BnagFer/domain/accounts/dto/social/UserSocialLoginRequestDto.java
@@ -1,14 +1,10 @@
 package com.capstone.BnagFer.domain.accounts.dto.social;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record UserSocialLoginRequestDto(
         @NotBlank(message = "[ERROR] 토큰 입력은 필수 입니다.")
         String accessToken,
-        @NotBlank(message = "[ERROR] 이메일 입력은 필수 입니다.")
-        @Schema(description = "email", example = "test1234@naver.com")
-        String email,
         String fcmToken
 ) {
 }
