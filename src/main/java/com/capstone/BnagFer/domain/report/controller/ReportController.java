ff6144d8c224055f6c4ba1c008cc3bbcaead5ad2
Geunsik Kim <93639883+600gramSik@users.noreply.github.com>
2024-07-14 16:58:21 +0900

    ✨FEATURE: SWAGGER 화면 상 staff 전용 API, 신고 API 제목 추가(#148) (#149)

--- src/main/java/com/capstone/BnagFer/domain/report/controller/ReportController.java
+++ src/main/java/com/capstone/BnagFer/domain/report/controller/ReportController.java
@@ -12,7 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@Tag(name = "staff 전용 API")
+@Tag(name = "신고 API")
 @RequestMapping("/report")
 @RequiredArgsConstructor
 public class ReportController{
