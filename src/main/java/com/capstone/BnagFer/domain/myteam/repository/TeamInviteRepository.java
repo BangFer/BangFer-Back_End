c856a9bff2c40d42efe2dec5974c35f56266b17c
Geunsik Kim <geunsikevin@naver.com>
2024-07-14 16:20:52 +0900

    ✨FEATURE: 내가 받은 초대 조회 코드 최종 리팩토링(#130)

--- src/main/java/com/capstone/BnagFer/domain/myteam/repository/TeamInviteRepository.java
+++ src/main/java/com/capstone/BnagFer/domain/myteam/repository/TeamInviteRepository.java
@@ -1,15 +1,11 @@
 package com.capstone.BnagFer.domain.myteam.repository;
 
 import com.capstone.BnagFer.domain.accounts.entity.User;
-import com.capstone.BnagFer.domain.myteam.entity.Team;
 import com.capstone.BnagFer.domain.myteam.entity.TeamInvite;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface TeamInviteRepository extends JpaRepository<TeamInvite, Long> {
