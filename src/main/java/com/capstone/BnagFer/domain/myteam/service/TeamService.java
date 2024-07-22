1e8e0c0579286449b96c6f4c22f7c5e93a0cfe6a
gomin0 <gomin0981113@naver.com>
2024-07-22 21:55:49 +0900

    :recycle: refactor: 팀 생성 시 팀이름이랑 적용전술 받아서 매핑 (#157)

--- src/main/java/com/capstone/BnagFer/domain/myteam/service/TeamService.java
+++ src/main/java/com/capstone/BnagFer/domain/myteam/service/TeamService.java
@@ -1,5 +1,5 @@
-
 package com.capstone.BnagFer.domain.myteam.service;
+
 import com.capstone.BnagFer.domain.accounts.entity.User;
 import com.capstone.BnagFer.domain.accounts.service.account.AccountsCommonService;
 import com.capstone.BnagFer.domain.myteam.dto.request.CUTeamRequestDto;
@@ -10,6 +10,9 @@ import com.capstone.BnagFer.domain.myteam.entity.TeamMember;
 import com.capstone.BnagFer.domain.myteam.exception.TeamExceptionHandler;
 import com.capstone.BnagFer.domain.myteam.repository.TeamMembersRepository;
 import com.capstone.BnagFer.domain.myteam.repository.TeamRepository;
+import com.capstone.BnagFer.domain.tactic.entity.Tactic;
+import com.capstone.BnagFer.domain.tactic.exception.TacticExceptionHandler;
+import com.capstone.BnagFer.domain.tactic.repository.TacticRepository;
 import com.capstone.BnagFer.global.common.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,17 +26,30 @@ public class TeamService {
     private final TeamRepository teamRepository;
     private final AccountsCommonService accountsCommonService;
     private final TeamMembersRepository teamMembersRepository;
+    private final TacticRepository tacticRepository;
 
     public CUTeamResponseDto createMyTeam(CUTeamRequestDto request, User user) {
 
-        Team team = request.toEntity();
+        // 프로필 존재 확인
+        accountsCommonService.checkUserProfile(user);
+
+        // 적용 가능 전술 유무 확인
+        if (user.getTactics().isEmpty()) {
+            throw new TeamExceptionHandler(ErrorCode.USER_TEAM_TACTIC_NOT_FOUND);
+        }
+
+
+        Tactic tactic = tacticRepository.findById(request.tacticId())
+                .orElseThrow(() -> new TacticExceptionHandler(ErrorCode.TACTIC_NOT_FOUND));
+
+        if (!user.getTactics().contains(tactic)) {
+            throw new TacticExceptionHandler(ErrorCode.TACTIC_NOT_ALLOWED);
+        }
+
+        Team team = request.toEntity(tactic);
         team.updateLeader(user);
         TeamMember teamMember = TeamMember.createTeamMember(user, Role.LEADER, team);
 
-        // 프로필 존재 확인
-        accountsCommonService.checkUserProfile(team.getLeader());
-
-        //teamMember.updateUserRoleAndTeam(user, team);
         teamMembersRepository.save(teamMember);
         teamRepository.save(team);
 
