3b1425b4453e0b4fae2a7d735bbf32947a0ebecb
Geunsik Kim <93639883+600gramSik@users.noreply.github.com>
2024-08-03 19:38:07 +0900

    [REFACTOR] 팀원 초대 시 닉네임으로 초대되게 끔 구현 (#166)
    * ♻️refactor: 팀 초대 시 사용자 닉네임으로 받게 구현 (#165)
    
    * 💄style:최종코드 수정 (#165)
    
    * 💄style: 최종코드 수정 (#165)
    
    * ♻️refactor: 불필요한 예외처리 제외 (#165)

--- src/main/java/com/capstone/BnagFer/domain/accounts/repository/UserJpaRepository.java
+++ src/main/java/com/capstone/BnagFer/domain/accounts/repository/UserJpaRepository.java
@@ -2,8 +2,6 @@ package com.capstone.BnagFer.domain.accounts.repository;
 
 import com.capstone.BnagFer.domain.accounts.entity.User;
 import com.capstone.BnagFer.domain.report.entity.UserActivity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
