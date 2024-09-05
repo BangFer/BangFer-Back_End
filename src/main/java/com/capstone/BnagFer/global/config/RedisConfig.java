ef5688fe566154a3a8844150a483647f9fea3b75
gomin0 <gomin0981113@naver.com>
2024-09-05 12:48:32 +0900

    :lipstick: style: import문 삭제 (#185)

--- src/main/java/com/capstone/BnagFer/global/config/RedisConfig.java
+++ src/main/java/com/capstone/BnagFer/global/config/RedisConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
