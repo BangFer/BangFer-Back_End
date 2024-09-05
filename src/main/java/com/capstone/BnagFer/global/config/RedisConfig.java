9ed3bb7000c78fe0fd7560322e70383c9eda88d1
gomin0 <gomin0981113@naver.com>
2024-09-05 12:47:34 +0900

    :recycle: refactor: 분산 락 적용 (#185)

--- src/main/java/com/capstone/BnagFer/global/config/RedisConfig.java
+++ src/main/java/com/capstone/BnagFer/global/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.capstone.BnagFer.global.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,5 +33,12 @@ public class RedisConfig {
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         return redisTemplate;
+    }
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://" + redisHost + ":" + redisPort);
+        return Redisson.create(config);
     }
 }
