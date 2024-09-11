fe56c37b10a2eb412e236d1add77c6aa32c66f5f
Geunsik Kim <geunsikevin@naver.com>
2024-09-11 15:20:12 +0900

    ♻️refactor: 사용자 차단 시, 차단 객체는 Dto가 아닌 Entity에서 처리되도록 static 메서드로 대체(#189)

--- src/main/java/com/capstone/BnagFer/domain/board/entity/BoardBlock.java
+++ src/main/java/com/capstone/BnagFer/domain/board/entity/BoardBlock.java
@@ -1,10 +1,10 @@
 package com.capstone.BnagFer.domain.board.entity;
-
 import com.capstone.BnagFer.domain.accounts.entity.User;
 import com.capstone.BnagFer.global.common.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDateTime;
+
 @Entity
 @Builder
 @Getter
@@ -16,15 +16,21 @@ public class BoardBlock extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "board_block_id", nullable = false)
     private Long id;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "block_user_id")
     private User blockUser;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "is_blocked_user_id")
     private User isBlockedUser;
 
     @Column(name = "blocked_at")
     private LocalDateTime blockedAt;
+
+    public static BoardBlock create(User blockUser, User isBlockedUser) {
+        BoardBlock boardBlock = new BoardBlock();
+        boardBlock.blockUser = blockUser;
+        boardBlock.isBlockedUser = isBlockedUser;
+        return boardBlock;
+
+    }
 }
--- src/main/java/com/capstone/BnagFer/domain/board/service/BoardBlockService.java
+++ src/main/java/com/capstone/BnagFer/domain/board/service/BoardBlockService.java
@@ -1,8 +1,6 @@
 package com.capstone.BnagFer.domain.board.service;
-
 import com.capstone.BnagFer.domain.accounts.entity.User;
 import com.capstone.BnagFer.domain.accounts.repository.UserJpaRepository;
-import com.capstone.BnagFer.domain.board.dto.request.BoardBlockRequestDto;
 import com.capstone.BnagFer.domain.board.dto.response.BoardBlockResponseDto;
 import com.capstone.BnagFer.domain.board.entity.BoardBlock;
 import com.capstone.BnagFer.domain.board.exception.BoardExceptionHandler;
@@ -11,7 +9,6 @@ import com.capstone.BnagFer.global.common.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -20,28 +17,31 @@ public class BoardBlockService {
     private final UserJpaRepository userRepository;
 
     public BoardBlockResponseDto blockUser(User blockUser, Long isBlockedUserId) {
-        User blockedUser = userRepository.findById(isBlockedUserId)
-                .orElseThrow(() -> new BoardExceptionHandler(ErrorCode.USER_NOT_FOUND));
-        if (blockUser.getId().equals(isBlockedUserId)) {
-            throw new BoardExceptionHandler(ErrorCode.CANNOT_REPORT_YOURSELF);
-        }
-        if(boardBlockRepository.existsByBlockUserAndIsBlockedUser(blockUser, blockedUser)){
+        User blockedUser = checkIfIsBlocked(isBlockedUserId);
+        validateSelfAction(blockUser, isBlockedUserId, ErrorCode.CANNOT_REPORT_YOURSELF);
+        if (boardBlockRepository.existsByBlockUserAndIsBlockedUser(blockUser, blockedUser)) {
             throw new BoardExceptionHandler(ErrorCode.ALREADY_BLOCKED);
         }
-        BoardBlockRequestDto request = new BoardBlockRequestDto();
-        BoardBlock boardBlock = request.toEntity(blockUser, blockedUser);
+
+        BoardBlock boardBlock = BoardBlock.create(blockUser, blockedUser);
         boardBlockRepository.save(boardBlock);
         return BoardBlockResponseDto.from(boardBlock);
     }
 
     public void unblockUser(User blockUser, Long isBlockedUserId) {
-        User blockedUser = userRepository.findById(isBlockedUserId)
-                .orElseThrow(() -> new BoardExceptionHandler(ErrorCode.USER_NOT_FOUND));
-        if (blockUser.getId().equals(isBlockedUserId)) {
-            throw new BoardExceptionHandler(ErrorCode.CANNOT_UNBLOCK_YOURSELF);
-        }
+        User blockedUser = checkIfIsBlocked(isBlockedUserId);
+        validateSelfAction(blockUser, isBlockedUserId, ErrorCode.CANNOT_UNBLOCK_YOURSELF);
         boardBlockRepository.deleteByBlockUserAndIsBlockedUser(blockUser, blockedUser);
     }
 
+    private User checkIfIsBlocked(Long isBlockedUserId) {
+        return userRepository.findById(isBlockedUserId)
+                .orElseThrow(() -> new BoardExceptionHandler(ErrorCode.USER_NOT_FOUND));
+    }
 
+    private void validateSelfAction(User blockUser, Long isBlockedUserId, ErrorCode errorCode) {
+        if (blockUser.getId().equals(isBlockedUserId)) {
+            throw new BoardExceptionHandler(errorCode);
+        }
+    }
 }
