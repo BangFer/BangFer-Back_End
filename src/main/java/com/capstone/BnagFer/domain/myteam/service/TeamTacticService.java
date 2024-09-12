8f973dadcdfb7ad8623a5eac8100556c4b932b54
Geunsik Kim <93639883+600gramSik@users.noreply.github.com>
2024-09-12 13:57:37 +0900

    [REFACTOR] MyTeam&Board 부분 클린코딩 적용(#189) (#193)
    * ♻️refactor: 사용자 차단 시, 차단 객체는 Dto가 아닌 Entity에서 처리되도록 static 메서드로 대체(#189)
    
    * ♻️refactor: 클린코딩 적용(#189)

--- src/main/java/com/capstone/BnagFer/domain/myteam/service/TeamTacticService.java
+++ src/main/java/com/capstone/BnagFer/domain/myteam/service/TeamTacticService.java
@@ -31,9 +31,6 @@ public class TeamTacticService {
         // 팀 멤버들의 포지션 초기화
         resetTeamMemberPositions(team);
 
-        // 팀 저장
-        teamRepository.save(team);
-
         return CreateTeamTacticResponseDto.from(team, CreateTeamTacticResponseDto.TacticDto.from(tactic));
     }
 
@@ -47,12 +44,10 @@ public class TeamTacticService {
         }
         if (team.getTactic() != null && team.getTactic().getTacticId().equals(tacticId)) {
             team.deleteMyTactic();
-            teamRepository.save(team);
         } else {
             throw new TacticExceptionHandler(ErrorCode.TACTIC_NOT_FOUND);
         }
     }
-
     private void validateAndUpdateTeam(Team team, User user, Tactic tactic) {
 
         // 팀 리더 검증
